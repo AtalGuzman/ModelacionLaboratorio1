5d9ab1b4a3e04d4dcbfb022dd24d2e105bf990f4
iibanez <ignacio.ibanez@usach.cl>
2017-04-02 16:03:28 -0300

    Se agrego valor absoluto

--- newton_raphson.m
+++ newton_raphson.m
@@ -2,7 +2,7 @@ function raiz = newton_raphson(polinomio, iteraciones, error, Xn)
     d_polinomio = polyder(polinomio); %se deriva el polinomio en función a x.
     
     Xn_1 = Xn - (polyval(polinomio, Xn)/polyval(d_polinomio, Xn)); %se calcula el nuevo valor de x
-    error_actual = (Xn_1-Xn)/Xn_1; %se calcula el valor actual para ver si es menor a la toleracia
+    error_actual = abs((Xn_1-Xn)/Xn_1); %se calcula el valor actual para ver si es menor a la toleracia
     
     if (error_actual <= error || iteraciones == 1) %si el error es menor al solicitado o la cantidad de iteraciones
         raiz = Xn_1;                               %termina retornar el valor de la raiz
