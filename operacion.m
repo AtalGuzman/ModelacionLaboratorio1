8ce94a579b86f5ff4576fd5035c0220b82876a24
iibanez <ignacio.ibanez@usach.cl>
2017-04-08 19:12:27 -0300

    solución problema en funcion operacion

--- operacion.m
+++ operacion.m
@@ -7,9 +7,9 @@ function resultado = operacion(vector)
     elseif(length(ordenar)< 4)%se revisa que el largo del vector no se inferior a 4
         fprintf('La cantidad de elementos del vector no es suficiente, mínimo 4 elementos.\n')
     else
-        menores = sqrt(ordenar(1:4)); %se obtienen los 4 menores numeros con su raiz respectiva
-        mayores = sqrt(ordenar(end-3:end)); %se obtienen los 4 mayores numeros con su raiz respectiva
-        resultado = sum(mayores) - sum(menores);% se obtiene el resultado a la operación
+        menores = sum(sqrt(ordenar(1:4))); %se obtienen los 4 menores numeros con su raiz respectiva y se suman
+        mayores = sqrt(sum(ordenar(end-3:end))); %se obtienen los 4 mayores numeros se suman y obtenemos su raiz cuadrada
+        resultado = mayores - menores;% se obtiene el resultado a la operación
     end
     
 end
